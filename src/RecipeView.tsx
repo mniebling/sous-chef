7c0638ec05fcb481cb8050480b63086d338768b6
Mike Niebling <michael.niebling@gmail.com>
2024-12-27 11:16:34 -0600

    Render markdown contents on the frontend

--- src/RecipeView.tsx
+++ src/RecipeView.tsx
@@ -1,3 +1,9 @@
+import { useEffect, useState } from 'react'
+import rehypeSanitize from 'rehype-sanitize'
+import rehypeStringify from 'rehype-stringify'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import { unified } from 'unified'
 import type { Recipe } from './typings/recipe'
 
 export interface RecipeViewProps {
@@ -6,9 +12,23 @@ export interface RecipeViewProps {
 
 export function RecipeView(props: RecipeViewProps) {
 
-	if (!props.recipe) return null
+	const [html, setHtml] = useState<string>('')
+
+	useEffect(() => {
+		if (!props.recipe?.content) return
+
+		unified()
+			.use(remarkParse) // convert the Markdown into an AST (mdast)
+			.use(remarkRehype) // convert the Markdown AST into an HTML AST (hast)
+			.use(rehypeSanitize) // remove dangerous tags
+			.use(rehypeStringify) // convert the HTML AST into an HTML string
+			.process(props.recipe.content)
+			.then(file => {
+				setHtml(String(file))
+			})
+	}, [props.recipe?.content])
 
 	return (
-		<pre>{ props.recipe.content }</pre>
+		<div dangerouslySetInnerHTML={{ __html: html }} />
 	)
 }
