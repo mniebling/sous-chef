bfc12b12ee491df86cff8f0ae3774944efbb016d
Mike Niebling <michael.niebling@gmail.com>
2025-02-07 11:59:55 -0600

    Add a screen lock on recipe view

--- src/RecipeView.tsx
+++ src/RecipeView.tsx
@@ -1,5 +1,6 @@
 import { invoke } from '@tauri-apps/api/core'
-import { useEffect, useState } from 'react'
+import classNames from 'classnames'
+import { useEffect, useRef, useState } from 'react'
 import rehypeSanitize from 'rehype-sanitize'
 import rehypeStringify from 'rehype-stringify'
 import remarkParse from 'remark-parse'
@@ -17,11 +18,39 @@ export function RecipeView(props: RecipeViewProps) {
 
 	const [html, setHtml] = useState<string>('')
 	const [recipe, setRecipe] = useState<Recipe | null>(null)
+	const [wakeLockError, setWakeLockError] = useState<string | null>(null)
+
+	const wakeLock = useRef<WakeLockSentinel | null>(null)
 
 	// TODO: Try out React 19 action hook here
 	useEffect(() => {
 		getRecipe(props.recipeHash).then(setRecipe)
 	}, [props.recipeHash])
+
+	// TODO: If this works well on-device, abstract this into a hook
+	useEffect(() => {
+		requestScreenWakeLock()
+
+		return () => {
+			releaseScreenWakeLock()
+		}
+	}, [])
+
+	async function requestScreenWakeLock() {
+
+		try {
+			wakeLock.current = await navigator.wakeLock.request('screen')
+		}
+		catch (err) {
+			setWakeLockError(`${err.name}, ${err.message}`)
+		}
+	}
+
+	async function releaseScreenWakeLock() {
+
+		await wakeLock.current?.release()
+		wakeLock.current = null
+	}
 
 	useEffect(() => {
 
@@ -49,6 +78,15 @@ export function RecipeView(props: RecipeViewProps) {
 			<main className={ css.recipeContainer }>
 				<pre>{ JSON.stringify(recipe?.metadata, null, 2) }</pre>
 				<div dangerouslySetInnerHTML={{ __html: html }} />
+
+				{ !wakeLock.current?.released && (
+					<div className={ classNames(css.screenLockStatus, { [css.isError]: !!wakeLockError }) }>
+						{ wakeLockError !== null
+							? <span>{ wakeLockError }</span>
+							: <span>Display lock: ON</span>
+						}
+					</div>
+				)}
 			</main>
 		</div>
 	)
